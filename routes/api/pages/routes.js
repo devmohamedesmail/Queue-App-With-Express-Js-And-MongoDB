721df6da7a33e4eb067a2179fcb138f015373fbe
Mohamed Esmail <div.mohamed.esmail@gmail.com>
2025-06-30 20:15:50 +0400

    Fix Error of import

--- routes/api/pages/routes.js
+++ routes/api/pages/routes.js
@@ -1,18 +1,18 @@
 import express from 'express'
-import { fetch_pages , update_page , delete_page, add_new_page } from '../../../controllers/api/pages_api_controller.js';
+// import { fetch_pages , update_page , delete_page, add_new_page } from '../../../controllers/api/pages_api_controller.js';
 const router = express.Router();
 
 
 
 
 
-router.post('/add/new/page', add_new_page)
-// ******** fetch all pages
-router.get('/' , fetch_pages)
-// edit Page
-router.post('/update/page/:pageId', update_page);
-// ********* delete page
-router.get('/delete/page/:pageId', delete_page);
+// router.post('/add/new/page', add_new_page)
+// // ******** fetch all pages
+// router.get('/' , fetch_pages)
+// // edit Page
+// router.post('/update/page/:pageId', update_page);
+// // ********* delete page
+// router.get('/delete/page/:pageId', delete_page);
 
 
 
