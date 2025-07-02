d963fdf983abfe36d849ecc32705924e3e6b8f45
Mohamed Esmail <div.mohamed.esmail@gmail.com>
2025-07-02 14:44:19 +0400

    Add Subscriber routes

--- routes/api/subscriber/routes.js
+++ routes/api/subscriber/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { get_Users_By_PlaceId ,add_new_user_to_place } from '../../../controllers/api/subscriber_api_controller.js';
+// import { get_Users_By_PlaceId ,add_new_user_to_place } from '../../../controllers/api/subscriber_api_controller.js';
+import { get_Users_By_PlaceId, add_new_user_to_place } from '../../../controllers/api/subscriber_controller.js';
 const router = express.Router();
 
-// get users by place
+// // get users by place
 router.get('/users/:placeId', get_Users_By_PlaceId)
 
-// add new user to place
+// // add new user to place
 router.post('/add/user/:placeId', add_new_user_to_place)
 
 
